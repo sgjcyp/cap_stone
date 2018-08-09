fd792aaa58fe323fa03c76e38d8311b5ea3bee72
Subhash Jaldu <subhash@Subhashs-MacBook-Air.local>
2018-08-09 14:38:31 -0700

    working pipeline py

--- airbnb_sfo/src/pipeline.py
+++ airbnb_sfo/src/pipeline.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import pickle
 import matplotlib.pyplot as plt
 # %matplotlib inline
 
@@ -8,29 +9,35 @@ from sklearn import datasets, linear_model
 from sklearn.metrics import  mean_squared_error, r2_score
 from collections import Counter
 
+from sklearn.ensemble import RandomForestRegressor
+# from sklearn.datasets import make_regression
+
+from sklearn.ensemble import GradientBoostingRegressor
+
 import statsmodels.api as sm
+
 
 
 def price_xform(dframe,colname):
     dframe[colname]=dframe[colname].str.replace('$','')
     dframe[colname]=dframe[colname].str.replace(',','')
     dframe[colname]=pd.to_numeric(dframe[colname])
-#     return(dframe)
+    #return(dframe)
 
 def cat_rename(dframe,colname,src,tgt):
     dframe[colname]=dframe[colname].str.replace(src,tgt)
-#     dframe[colname]=dframe[colname].str.replace(',','')
-#     dframe[colname]=pd.to_numeric(dframe[colname])
-#     return(dframe)
+    #     dframe[colname]=dframe[colname].str.replace(',','')
+    #     dframe[colname]=pd.to_numeric(dframe[colname])
+    #     return(dframe)
 
 def create_dummies(dframe,colname):
-#     dfnew=dframe.copy()
+    #     dfnew=dframe.copy()
     dframe=pd.get_dummies(data=dframe, columns=[colname])# create dummies and drop the parent column
     dframe.drop(columns=[dframe.columns[-1]],inplace=True) #dropped the last column from the add dummies
     return dframe
 
 def impute_nullrows(dframe,colname):
-#Impute Null Rows based on the specified column
+    #Impute Null Rows based on the specified column
     print(dframe[colname].isnull().sum())
     dftmp=dframe[~dframe[colname].isnull()]   # remove nulls from one column
     print(dftmp[colname].isnull().sum())
@@ -201,6 +208,7 @@ def create_database(dfclean):
 
     dfclean_lt_500=dfclean[dfclean.price<500]
     dfclean_gt_500=dfclean[dfclean.price>500]
+    dfclean_gt_500=dfclean_gt_500[dfclean_gt_500.price<1500]
 
     y=dfclean_lt_500.price
     X=dfclean_lt_500.copy()
@@ -224,8 +232,232 @@ def create_database(dfclean):
     y_test.to_pickle('pklz/price_split/y_gt_test.pkl')
 
 
+def regr_plot(y_act,y_pred,mdl_data):
+    #Building Residual DF
+
+    dfpred= y_act.to_frame()
+    dfpred['preds'] = y_pred
+    dfpred['resid'] = dfpred.preds-dfpred.price
+    dfpred['residpct'] = (dfpred.preds-dfpred.price)/dfpred.price
+    # dfpred.head()
+
+    fig = plt.figure(figsize=(18,5))
+    ax1 = fig.add_subplot(1,3,1)
+    ax2 = fig.add_subplot(1,3,2)
+    ax3 = fig.add_subplot(1,3,3)
+
+    ax1.scatter(y_act, y_pred,  color='black')
+    ax1.set_title('act_vs_pred')
+    ax1.set_xlabel('ACT-PRICE')
+    ax1.set_ylabel('PREDICT')
+
+    ax2.scatter(dfpred.price,dfpred.resid, color='red')
+    ax2.set_title('RESIDUALS')
+    ax2.set_xlabel('PRICE')
+    ax2.set_ylabel('RESIDUAL')
+
+    ax3.scatter(dfpred.price,dfpred.residpct, color='blue')
+    ax3.set_title('RESIDUAL PCT')
+    ax3.set_xlabel('PRICE')
+    ax3.set_ylabel('RESIDUAL PCT')
+
+    fig.suptitle('mdl_data', fontsize=12, y=1.2)
+    fig.tight_layout()
+
+    fig1 = plt.gcf()
+    fig1.savefig('plots/'+'RESI_'+mdl_data+'.png', format='png')
+
+def plot_feature_importance(featurelist,featureimp,name):
+    tmp_df=featurelist.to_frame(index=False)
+    tmp_df=tmp_df.rename(columns={0:'feature'})
+    tmp_df['prime']=featureimp
+    feature_df=tmp_df.sort_values(by=['prime'], ascending=True)
+    fig=plt.figure(figsize=(10,15))
+    ax1=fig.add_subplot(111)
+    ax1.barh(feature_df['feature'],feature_df['prime'],color='rgbkymc')
+    fig.suptitle(name, fontsize=12, y=1.2)
+    fig.tight_layout()
+    fig1 = plt.gcf()
+    fig1.savefig('plots/'+'FI_'+name+'.png', format='png')
+
+
+def xtra():
+
+    # X_train= pd.read_pickle('pklz/price_split/X_gt_train.pkl')
+    # y_train= pd.read_pickle('pklz/price_split/y_gt_train.pkl')
+    # X_test= pd.read_pickle('pklz/price_split/X_gt_test.pkl')
+    # y_test= pd.read_pickle('pklz/price_split/y_gt_test.pkl')    
+    # X_train=sm.add_constant(X_train,has_constant='add')
+    # X_test=sm.add_constant(X_test,has_constant='add')
+    # print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
+    # print(X_train.head().T)
+    # print(X_test.head().T)
+
+    # listd_insig_cols = ['host_is_superhost','host_identity_verified','bed_type','guests_included','room_type_full',\
+    #                 'room_type_pvt','amentcnt','ppt_condensed_apt','ppt_condensed_aptspl','ppt_condensed_auto',\
+    #                 'ppt_condensed_hotel','ppt_condensed_other',\
+                    # 'zipcode_94014.0', 'zipcode_94015.0', 'zipcode_94102.0',\
+                    # 'zipcode_94103.0', 'zipcode_94104.0', 'zipcode_94105.0',\
+                    # 'zipcode_94107.0', 'zipcode_94108.0', 'zipcode_94109.0',\
+                    # 'zipcode_94110.0', 'zipcode_94111.0', 'zipcode_94112.0',\
+                    # 'zipcode_94114.0', 'zipcode_94115.0', 'zipcode_94116.0',\
+                    # 'zipcode_94117.0', 'zipcode_94118.0', 'zipcode_94121.0',\
+                    # 'zipcode_94122.0', 'zipcode_94123.0', 'zipcode_94124.0',\
+                    # 'zipcode_94127.0', 'zipcode_94129.0', 'zipcode_94131.0',\
+                    # 'zipcode_94132.0', 'zipcode_94133.0', 'zipcode_94134.0','zipcode_94158.0']
+    # listd_insig_cols = ['zipcode_94014.0', 'zipcode_94102.0',\
+    #                 'zipcode_94103.0', 'zipcode_94104.0', 'zipcode_94105.0',\
+    #                 'zipcode_94107.0', 'zipcode_94108.0', 'zipcode_94109.0',\
+    #                 'zipcode_94110.0', 'zipcode_94112.0',\
+    #                 'zipcode_94114.0', 'zipcode_94115.0',\
+    #                 'zipcode_94015.0', 'zipcode_94111.0', 'zipcode_94116.0',\
+    #                 'zipcode_94117.0', 'zipcode_94118.0', 'zipcode_94121.0',\
+    #                 'zipcode_94122.0', 'zipcode_94123.0', 'zipcode_94124.0',\
+    #                 'zipcode_94127.0', 'zipcode_94129.0', 'zipcode_94131.0',\
+    #                 'zipcode_94132.0', 'zipcode_94133.0', 'zipcode_94134.0','zipcode_94158.0',\
+    #                 'ppt_condensed_apt','ppt_condensed_aptspl','ppt_condensed_auto',\
+    #                 'ppt_condensed_hotel','ppt_condensed_other','ppt_condensed_hostel','ppt_condensed_house',\
+    #                 'min_night_stay_long','min_night_stay_mid']
+    # X_train.drop(listd_insig_cols,axis=1,inplace=True)
+    # X_test.drop(listd_insig_cols,axis=1,inplace=True)
+    # print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
+
+
+    # modelOLS = sm.OLS(y_train, X_train)
+    # resultsOLS = modelOLS.fit()
+    # print(resultsOLS.summary())
+    # y_predOLS = resultsOLS.predict(X_test)
+    # my_metrics(y_test,y_predOLS,'OLS_LinReg_gt500_Dataset')
+    # regr_plot(y_test,y_predOLS,'OLS_LinReg_gt500_Dataset')
+
+
+    # #SK Learn Linreg
+    # lmodel = linear_model.LinearRegression()
+    # lmodel.fit(X_train,y_train)
+    # y_predsk = lmodel.predict(X_test)
+    # print('Coefficients: \n', lmodel.coef_)
+    # my_metrics(y_test,y_predsk,'SKLearn Linear Reg Metrics')
+    pass
+
+def my_metrics(y_act,y_pred,mdl_data):
+    r2= (1 - (((y_act-y_pred)**2).sum()) / (((y_act-y_act.mean())**2).sum()) )
+    r2_score= (1 - ((y_act-y_pred)**2).sum() / ((y_act-y_act.mean())**2).sum() )
+    mse =  ((y_act-y_pred) ** 2).sum()/len(y_act)
+    rmse = mse ** 0.5
+    rmsle = np.sqrt(np.mean((np.log(1+y_pred) - np.log(1+y_act))**2))
+    mspct = np.abs(((y_act-y_pred)/y_act)).mean()
+
+
+    mse_prop = (np.mean(np.abs((y_act - y_pred)**2 / (y_act+1)**2  )) * 100) **0.5
+
+    #Calculate the baseline errors
+    baseline = abs(y_act.mean()-y_act)
+    #Calculate the absolute errors
+    residuals = abs(y_pred-y_test)
+    #Calculate Mean Absolute pct error
+    mape = (residuals/y_test)*100
+    #Accuracy 
+    accy=100-np.mean(mape)
+    #Mean Absolute Error 
+    
+    print('\n**************** Metrics for : ',mdl_data,' *******************')
+    print("   R2 (Variance Squared ERROR)                   = %.4f"%r2)
+    print("   MSE (Mean Squared ERROR)                      = %.4f"%mse)
+    print("   RMSE (ROOT Mean Squared ERROR)                = %.4f"%rmse)
+    print("   RMSLE (ROOT Mean Squared Logrithmic ERROR)    = %.4f"%rmsle)
+    print("   PCTE  (Percent Absolute Error)                = %.4f\n"%mspct)
+
+    print("   Baseline Mean Absolute Error                  =",np.mean(baseline))
+    print("   Predicted Mean Absolute Error                 =",np.mean(residuals))
+    print("   Accuracy  (100-MAPE mean absolute PCT ERR)    =",round(accy,2), '%')
+
+    print('\n**************** -------------------------- *******************',len(y_act), mse_prop)
+    # print(len(y_act), mse_prop)
+
+    # regr_plot(y_act,y_pred,mdl_data)
+    return(r2,rmse,rmsle,mspct)
+
+def run_randforest(X_train,y_train,X_test,y_test,name):
+    print('\nRUNNING RANDOM FOREST .................  :',name)
+
+    regr = RandomForestRegressor(max_depth=2, random_state=0)
+    regr.fit(X_train, y_train)
+    RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=25,
+           max_features='auto', max_leaf_nodes=None,
+           min_impurity_decrease=0.0, min_impurity_split=None,
+           min_samples_leaf=1, min_samples_split=2,
+           min_weight_fraction_leaf=0.0, n_estimators=100, n_jobs=1,
+           oob_score=False, random_state=0, verbose=0, warm_start=False)
+
+    print(regr.predict([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]))
+    y_predRF=regr.predict(X_test)
+    my_metrics(y_test,y_predRF,name)
+
+    featurelist=X_train.columns
+    featureimp= regr.feature_importances_
+    plot_feature_importance(featurelist,featureimp,name)
+    regr_plot(y_test,y_predRF,name)
+
+
+
+
+def run_gradientboost(X_train,y_train,X_test,y_test,name):
+    print('\nRUNNING GRADIENT BOOST REGRESSION .................  :',name)
+    params = {'n_estimators': 500, 'max_depth': 4, 'min_samples_split': 2,\
+          'learning_rate': 0.01, 'loss': 'ls', 'random_state':0}
+
+    gbr = GradientBoostingRegressor(**params)
+    # gbr = GradientBoostingRegressor(max_depth=2, random_state=0)
+    gbr.fit(X_train, y_train)
+
+    y_predGBR = gbr.predict(X_test)
+    mse= mean_squared_error(y_test,y_predGBR)
+    print("MSE: %.4f" %mse)
+    # print("My GBR Score : %.4f",my_metric(y_test,y_predGBR))
+
+    print(gbr.predict([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]))
+    y_pred=gbr.predict(X_test)
+    my_metrics(y_test,y_pred,name)
+
+    featurelist=X_train.columns
+    featureimp= gbr.feature_importances_
+    plot_feature_importance(featurelist,featureimp,name)
+    regr_plot(y_test,y_pred,name)
+
+
+
+
+
+def run_linreg(X_train,y_train,X_test,y_test,name):
+    print('\nRUNNING LINEAR REGRESSION .................  : ',name)   
+    X_train=sm.add_constant(X_train,has_constant='add')
+    X_test=sm.add_constant(X_test,has_constant='add')
+    # print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
+    # print(X_train.head().T)
+    # print(X_test.head().T)
+
+    # Statmodel Linreg
+    modelOLS = sm.OLS(y_train, X_train)
+    resultsOLS = modelOLS.fit()
+    print(resultsOLS.summary())
+    y_predOLS = resultsOLS.predict(X_test)
+    my_metrics(y_test,y_predOLS,name)
+    regr_plot(y_test,y_predOLS,name)
+
+
+
 if __name__ == '__main__':
     init()
-    df = read_data()
-    df = process_data(df)
-    create_database(df)
+
+    # df = read_data()
+    # df = process_data(df)
+    # create_database(df)
+
+    X_train= pd.read_pickle('pklz/price_split/X_lt_train.pkl')
+    y_train= pd.read_pickle('pklz/price_split/y_lt_train.pkl')
+    X_test= pd.read_pickle('pklz/price_split/X_lt_test.pkl')
+    y_test= pd.read_pickle('pklz/price_split/y_lt_test.pkl') 
+
+    run_linreg(X_train,y_train,X_test,y_test,'OLS_LinReg_lt500_Dataset')
+    run_randforest(X_train,y_train,X_test,y_test,'RF_lt500_Dataset')
+    run_gradientboost(X_train,y_train,X_test,y_test,'GB_lt500_Dataset')
